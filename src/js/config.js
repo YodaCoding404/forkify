54f1f4f3faeee0d366eedc345688943eb01b4113
Spades <thenightking011@gmail.com>
2024-08-19 09:46:04 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e65abad3-6b77-4953-b413-7aed674ab3b2';
+export const MODAL_CLOSE_SEC = 2.5;
